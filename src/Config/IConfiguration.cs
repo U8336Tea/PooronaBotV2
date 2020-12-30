0c499b9c58a9dd954a4655c42add17a2023ed27a
U8336Tea <gogglinmike@gmail.com>
2020-12-30 14:42:50 -0800

    Added IList support for Config.

--- src/Config/IConfiguration.cs
+++ src/Config/IConfiguration.cs
@@ -1,27 +1,38 @@
 using System;
+using System.Collections;
 
 namespace PooronaBot.Config
 {
     public interface IConfiguration
     {
-        abstract string Get(string key);
+        abstract object Get(string key);
         abstract void Set(string key, object value);
+
+        string GetString(string key)
+        {
+            return this[key].ToString();
+        }
 
         int GetInt(string key)
         {
-            return int.Parse(this[key]);
+            return int.Parse(this.GetString(key));
         }
 
         float GetFloat(string key)
         {
-            return float.Parse(this[key]);
+            return float.Parse(this.GetString(key));
         }
 
         DateTime GetDateTime(string key)
         {
-            return DateTime.Parse(this[key]);
+            return DateTime.Parse(this.GetString(key));
         }
 
-        public string this[string key] { get => Get(key); set => Set(key, value); }
+        IList GetList(string key)
+        {
+            return (IList)this[key];
+        }
+
+        public object this[string key] { get => Get(key); set => Set(key, value); }
     }
 }
