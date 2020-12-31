ee7a75a0b310beb5fc0b2b61c77856c1249e3f7a
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2020-12-31 12:28:28 -0800

    Minor message change.

--- src/Exceptions/CuredException.cs
+++ src/Exceptions/CuredException.cs
@@ -4,7 +4,7 @@ namespace PooronaBot.Exceptions
     [System.Serializable]
     public class CuredException : System.Exception
     {
-        public CuredException() : this("The vaccine has rendered this person immune to the virus!") { }
+        public CuredException() : this("This user took the vaccine like some sort of commie.") { }
         public CuredException(string message) : base(message) { }
         public CuredException(string message, System.Exception inner) : base(message, inner) { }
         protected CuredException(
